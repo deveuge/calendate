26dd5a2ac946f53efe46212a2827d442da185706
Eugenia <48625552+deveuge@users.noreply.github.com>
2021-09-18 10:36:16 +0200

    Fixes for Heroku deployment SQL postgre corrections Modify Gmail port

--- src/main/java/com/deveuge/calendate/model/dao/IEventDAO.java
+++ src/main/java/com/deveuge/calendate/model/dao/IEventDAO.java
@@ -16,7 +16,7 @@ public interface IEventDAO extends CrudRepository<Event, Long> {
 	List<Event> findAllByEventType_EventTypeIdUserUsernameAndEventType_EventTypeIdUrl(String username, String url);
 	List<Event> findAllByEventType_EventTypeIdUserUsernameAndDateFromBetweenOrderByDateFrom(String username, Date dateFrom, Date dateTo);
 	
-	@Query(value = "SELECT to_char(e.dateFrom, 'dd'), COUNT(e.id) FROM Event e WHERE (dateFrom >= :dateFrom AND dateFrom <= :dateTo) AND username_id = :username GROUP BY e.dateFrom")
+	@Query(value = "SELECT to_char(e.dateFrom, 'dd'), COUNT(e.id), date_trunc('day', e.dateFrom) FROM Event e WHERE (dateFrom >= :dateFrom AND dateFrom <= :dateTo) AND username_id = :username GROUP BY 1,3 ORDER BY 3")
 	List<Object[]> countAllBetweenDates(@Param(value = "username") String username, @Param(value = "dateFrom") Date dateFrom, @Param(value = "dateTo") Date dateTo);
 	
 	@Query(value = "SELECT new com.deveuge.calendate.view.dto.AvailableHoursDTO(to_char(e.dateFrom, 'HH:mm'), to_char(e.dateTo, 'HH:mm')) FROM EventType t " + 
